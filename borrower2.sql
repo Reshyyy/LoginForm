92081cf9f6cd4a6e11ca1000ef07cf4baa438abd
Reshyyy <riyadhbrailleanthonyligan@gmail.com>
2023-04-02 14:17:59 +0800

    Changes: Dropped table BorrowBook and BorrowBook1 Reason: Changed Data type of borrow_date and return_date to datetime.

--- borrower2.sql
+++ borrower2.sql
@@ -25,6 +25,7 @@ return_date datetime
 );
 
 drop table BorrowBook
+drop table BorrowBook1
 
 select * from tblUserRegistration
 select * from books
@@ -33,8 +34,8 @@ select * from BorrowBook2
 
 INSERT INTO BorrowBook2 (std_name, std_number, std_dept, std_contact, std_email, book_name, borrow_date, return_date)
 VALUES
-    ('John Smith', '123456789', 'Biology', 5551234567, 'johnsmith@example.com', 'The Great Gatsby', '2023-03-15', '2023-04-05'),
-    ('Jane Doe', '987654321', 'History', 5559876543, 'janedoe@example.com', 'To Kill a Mockingbird', '2023-03-17', '2023-04-07'),
-    ('Bob Johnson', '456789123', 'Computer Science', 5554567890, 'bobjohnson@example.com', '1984', '2023-03-19', '2023-04-09'),
-    ('Sarah Lee', '321654987', 'Psychology', 5553216540, 'sarahlee@example.com', 'Pride and Prejudice', '2023-03-18', '2023-04-08'),
-    ('David Kim', '741852963', 'English', 5557418520, 'davidkim@example.com', 'The Catcher in the Rye', '2023-03-16', '2023-04-06');
+    ('John Smith', '123456789', 'Biology', 5551234567, 'johnsmith@example.com', 'The Great Gatsby', '2023-03-15', null),
+    ('Jane Doe', '987654321', 'History', 5559876543, 'janedoe@example.com', 'To Kill a Mockingbird', '2023-03-17', null),
+    ('Bob Johnson', '456789123', 'Computer Science', 5554567890, 'bobjohnson@example.com', '1984', '2023-03-19', null),
+    ('Sarah Lee', '321654987', 'Psychology', 5553216540, 'sarahlee@example.com', 'Pride and Prejudice', '2023-03-18', null),
+    ('David Kim', '741852963', 'English', 5557418520, 'davidkim@example.com', 'The Catcher in the Rye', '2023-03-16', null);
